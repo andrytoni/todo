54d9f57de156fe190beb98ce8c62fa9c77a8c727
André Luiz Fernandes de Brito <andre.brito.fernandes@gmail.com>
2026-01-04 14:08:21 -0300

    add boolean field done to item model

---
+++ prisma/migrations/20260104164754_add_done_field_items/migration.sql
@@ -0,0 +1,18 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Item" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "content" TEXT NOT NULL,
+    "observations" TEXT,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "listId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Item_listId_fkey" FOREIGN KEY ("listId") REFERENCES "List" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Item" ("content", "createdAt", "id", "listId", "observations", "updatedAt") SELECT "content", "createdAt", "id", "listId", "observations", "updatedAt" FROM "Item";
+DROP TABLE "Item";
+ALTER TABLE "new_Item" RENAME TO "Item";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
